6d256c098a8978d5991b507ff151ce5c4310ae91
Cihan Emre Kisakurek <cihan.kisakurek@erasys.de>
2022-01-10 22:34:03 +0100

    Initial Commit

---
+++ PreaApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PreaApp
+//
+//  Created by Cihan Emre Kisakurek on 10.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PreaApp/PreaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreaAppApp.swift
+//  PreaApp
+//
+//  Created by Cihan Emre Kisakurek on 10.01.22.
+//
+
+import SwiftUI
+
+@main
+struct PreaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
